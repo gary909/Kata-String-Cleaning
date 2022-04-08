c170bed2f4616099ab1bb5c4d95d2dbffc838f15
gary <gary.white@lodgit.com>
2022-04-08 12:48:02 +0200

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function stringClean(s) {
+    // Function will return the cleaned string
+}
+
+console.log(stringClean("")); // ""
+console.log(stringClean("! !")); // "! !"
+console.log(stringClean("123456789")); // ""
+console.log(stringClean("(E3at m2e2!!)")); // "(Eat me!!)"
+console.log(stringClean("Dsa32 cdsc34232 csa!!! 1I 4Am cool!")); // "Dsa cdsc csa!!! I Am cool!"
+console.log(stringClean("A1 A1! AAA   3J4K5L@!!!")); // "A A! AAA   JKL@!!!"
